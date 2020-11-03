8458b6160478c10861095c7ffa5db0694fb9c577
manfred.madelaine <manfred.madelaine@outlook.fr>
2020-11-04 00:44:22 +0100

    :sparkles: Store grid size and implement next game iteration

--- game_of_life.py
+++ game_of_life.py
@@ -1,7 +1,6 @@
-
 # Model
-# grid : update 
-# print grid
+# grid : update
+# [x] print grid
 
 # Core
 # Update grid
@@ -15,50 +14,87 @@
 # history
 # save/import figures
 
+
 class GameOfLife:
-	grid  = []
+    grid = []
+    rows = 0
+    cols = 0
 
-	# next()
+    def __str__(self):
+        alive_char = "O"
+        dead_char = "-"
 
-	def __str__(self):
-		alive_char = 'O'
-		dead_char = '-'
+        txt = ""
+        for row in self.grid:
+            txt += "\n"
+            for cell in row:
+                txt += alive_char if cell else dead_char
 
-		txt = ''
-		for row in self.grid:
-			txt += '\n'
-			for cell in row:
-				txt += alive_char if cell else dead_char
+        return "The Great Game of Life !" + txt
 
-		return "The Great Game of Life !" + txt
+    def populate_grid(self, txt_grid, alive_char):
+        for line in txt_grid:
+            row = []
+            for c in line:
+                row += [c == alive_char]
+            self.grid += [row]
 
-	def populate_grid(self, txt_grid, living_cell_char):
-		print(txt_grid)
-		for line in txt_grid:
-			row = []
-			for c in line:
-				row += [c == living_cell_char]
-			self.grid += [row]
+        self.set_size()
+
+    def set_size(self):
+        self.rows = len(self.grid)
+
+        row_sizes = [len(r) for r in self.grid]
+        if len(set(row_sizes)) > 1:
+            self._correct_size_mismatch(max(row_sizes))
+        self.cols = len(self.grid[0])
+
+    def _correct_size_mismatch(self, max_len):
+        for row in self.grid:
+            if len(row) < max_len:
+                row += [False]              
+        print("Size mismatch resolved !")
+
+    def next(self):
+        next_grid = []
+
+        for i in range(self.rows):
+            row = []
+            for j in range(self.cols):
+                neighbors = self.get_neighbors(i, j)
+                next_state = self.predict_next_state(neighbors)
+                row += [next_state]
+            next_grid += [row]
+
+        self.grid = next_grid
+
+    def get_neighbors(self, i, j):
+        pass
+
+    def predict_next_state(self, neighbors):
+        pass
 
 
 def txt_to_list(txt):
-	return txt.replace('\t', '').split('\n')[1:-1]
+    return txt.replace(" ", "").split("\n")[1:-1]
+
 
 def main():
-	test = """
-	.......
-	.......
-	...#...
-	.......
-	.......
-	"""
-	
-	test_list = txt_to_list(test)
-	print(test_list)
+    test = """
+    .......
+    .......
+    ...#...
+    .......
+    .......
+    """
 
-	gol = GameOfLife()
-	gol.populate_grid(test_list, "#")
+    test_list = txt_to_list(test)
 
-	print(gol)
+    gol = GameOfLife()
+    gol.populate_grid(test_list, "#")
+    print(gol)
+
+    # gol.next()
+
 
 main()
