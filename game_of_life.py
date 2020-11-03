ea2a36eb54556beb64deb0228caf17bc803a2ea5
manfred.madelaine <manfred.madelaine@outlook.fr>
2020-11-04 00:20:15 +0100

    :tada: Read from txt and generate grid

--- game_of_life.py
+++ game_of_life.py
@@ -16,12 +16,33 @@
 # save/import figures
 
 class GameOfLife:
-	# init
-	# grid
-	# next()
-	# 
-	pass
+	grid  = []
 
+	# next()
+
+	def __str__(self):
+		alive_char = 'O'
+		dead_char = '-'
+
+		txt = ''
+		for row in self.grid:
+			txt += '\n'
+			for cell in row:
+				txt += alive_char if cell else dead_char
+
+		return "The Great Game of Life !" + txt
+
+	def populate_grid(self, txt_grid, living_cell_char):
+		print(txt_grid)
+		for line in txt_grid:
+			row = []
+			for c in line:
+				row += [c == living_cell_char]
+			self.grid += [row]
+
+
+def txt_to_list(txt):
+	return txt.replace('\t', '').split('\n')[1:-1]
 
 def main():
 	test = """
@@ -31,7 +52,13 @@ def main():
 	.......
 	.......
 	"""
-	print(test)
+	
+	test_list = txt_to_list(test)
+	print(test_list)
 
+	gol = GameOfLife()
+	gol.populate_grid(test_list, "#")
+
+	print(gol)
 
 main()
