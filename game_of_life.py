bda25ca709bed87115c92c4b6d6f2523c0d2b493
manfred.madelaine <manfred.madelaine@outlook.fr>
2020-11-03 23:56:41 +0100

    :construction: Defining big steps

---
+++ game_of_life.py
@@ -0,0 +1,37 @@
+
+# Model
+# grid : update 
+# print grid
+
+# Core
+# Update grid
+# parse old and populate new state
+
+# track only living cells
+
+# Test place one cell and see it evolve
+# Display module
+
+# history
+# save/import figures
+
+class GameOfLife:
+	# init
+	# grid
+	# next()
+	# 
+	pass
+
+
+def main():
+	test = """
+	.......
+	.......
+	...#...
+	.......
+	.......
+	"""
+	print(test)
+
+
+main()
